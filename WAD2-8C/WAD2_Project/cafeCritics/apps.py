49d7d8fcb07e47ff6f3be2553b0c4e4c0b683796
Jasem <2961000A@student.gla.ac.uk>
2025-03-22 16:25:14 +0000

    comments for apps.py

--- WAD2-8C/WAD2_Project/cafeCritics/apps.py
+++ WAD2-8C/WAD2_Project/cafeCritics/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
-
+# Configuration class for the 'cafe' app
+# This class is used by Django to conifgure app-specific settings
+# and is referenced in the projects settings to register the 'cafe'
 class CafeConfig(AppConfig):
     name = 'cafe'
