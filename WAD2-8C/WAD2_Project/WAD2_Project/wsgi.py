27c30ae6952a9bc774e9b13ec53cfcef6bd9ba23
“Martin <2888769g@student.gla.ac.uk>
2025-03-27 19:11:02 +0000

    python anywhere fix

--- WAD2-8C/WAD2_Project/WAD2_Project/wsgi.py
+++ WAD2-8C/WAD2_Project/WAD2_Project/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for WAD2_Project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
+# filepath: untitled:/var/www/martingubala_pythonanywhere_com_wsgi.py
 import os
+import sys
 
+# Correct the path to your project directory
+sys.path.append('/home/martingubala/WAD2-8C/WAD2_Project')
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'WAD2_Project.settings'
+
+# Activate the virtual environment
+activate_this = '/home/martingubala/.virtualenvs/yourenvname/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Import and set up the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD2_Project.settings')
-
 application = get_wsgi_application()
