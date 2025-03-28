39578c90183d84a27ba166a2cc2cb39698a426f8
Jasem <2961000A@student.gla.ac.uk>
2025-03-28 01:16:55 +0000

    updateWSIG

--- WAD2-8C/WAD2_Project/WAD2_Project/wsgi.py
+++ WAD2-8C/WAD2_Project/WAD2_Project/wsgi.py
@@ -1,12 +1,12 @@
 # filepath: untitled:/var/www/martingubala_pythonanywhere_com_wsgi.py
-import os
-import sys
+# import os
+# import sys
 
 # Correct the path to your project directory
-sys.path.append('/home/martingubala/WAD2-8C/WAD2_Project')
+# sys.path.append('/home/martingubala/WAD2-8C/WAD2_Project')
 
 # Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'WAD2_Project.settings'
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'WAD2_Project.settings'
 
 # Activate the virtual environment
 # activate_this = '/home/martingubala/.virtualenvs/yourenvname/bin/activate_this.py'
@@ -14,5 +14,20 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'WAD2_Project.settings'
 #     exec(file_.read(), dict(__file__=activate_this))
 
 # Import and set up the WSGI application
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+
+import os
+import sys
+
+# Set the path to the project root
+path = '/home/Jasoom/WAD2-8C/WAD2-8C/WAD2_Project'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the environment variable for Django's settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'WAD2_Project.settings'
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
